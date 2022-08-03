c55751e52a0cd96118696e66bfc237d75046fc66
Sam Verdon <sam.verdon@btinternet.com>
2022-08-03 13:00:47 +0100

    very fast count primes to 250001

--- app/Main.hs
+++ app/Main.hs
@@ -1,7 +1,17 @@
 module Main where
 
 import Lib
+import System.Environment
 
 main :: IO ()
 main = do
-    putStrLn $ show $ smallPrimes 50
+    putStrLn $ show $ numPrimes [2..250001]
+
+ps :: (Integral a) => [a]
+ps = primesTo $ isqrt 250001
+
+numPrimes :: (Integral a) => [a] -> a
+numPrimes xs = foldl (+) 0 $ map f $ map (checkSmallList ps) xs
+    where f b
+            | b == True  = 1 
+            | b == False = 0
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,23 +1,43 @@
 module Lib
     ( checkSmall
+    ,checkSmallList
     ,smallPrimes
+    ,divisible
+    ,isqrt
+    ,primesTo
     ) where
 
 import Data.List
 
 seive :: (Integral a) => [a] -> [a]
-seive (p:xs) = p : seive [x | x <- xs, x `mod` p /= 0]
+seive (p:xs) = p : seive [ x | x <- xs, x `mod` p /= 0 ]
 
-smallPrimes :: (Integral a) => Int -> [a]
+smallPrimes :: (Integral a) => a -> [a]
 smallPrimes n
   | n < 1 = []
-  | otherwise = take n $ seive [2..]
+  | otherwise = genericTake n $ seive [2..]
 
-checkSmall :: (Integral a) => a -> Bool
-checkSmall p = not . foldl (||) False $ map (divisible p) (smallPrimes 50)
+primesTo :: (Integral a) => a -> [a]
+primesTo n
+  | n <1 = []
+  | otherwise = takeWhile (<= n) $ seive [2..]
+
+checkSmall :: (Integral a) => a -> a -> Bool
+checkSmall n p = null [ x | x <- smallPrimes n, p `mod` x == 0, p /= x ]
+
+checkSmallList :: (Integral a) => [a] -> a -> Bool
+checkSmallList ns p = null [ x | x <- ns, p `mod` x == 0, p /= x ]
 
 divisible :: (Integral a) => a -> a -> Bool
 divisible a b
   | a == b          = False
   | a `mod` b /= 0  = False
   | otherwise       = True
+
+isqrt :: (Integral a) => a -> a
+isqrt 0 = 0
+isqrt 1 = 1
+isqrt n = head $ dropWhile (\x -> x*x > n) $ iterate (\x -> (x + n `div` x ) `div` 2) (n `div` 2)
+
+trialDivision :: (Integral a) => a -> Bool
+trialDivision p = if p > 1 then null [ x | x <- [2..isqrt p], p `mod` x == 0] else False
